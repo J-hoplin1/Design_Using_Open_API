04bc617472b892e62e534b41093fa8c93a30010a
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-08 20:08:36 +0900

    get Data from api

--- GetDataFromOpenAPI/getDatas.py
+++ GetDataFromOpenAPI/getDatas.py
@@ -20,7 +20,7 @@ encoding의 기본값은 'utf-8'입니다. errors의 기본값은 'replace'로, 
 '''
 
 
-apiKey = unquote('bj9OInFd8JfcavWNdVhUfOLalfpaYG1N6wqkFTbKVzPwR0EkEj5pL55HrsPX6Nye4gREdN3InXTi2pv39h%2FgTQ%3D%3D')
+apiKey = unquote('')
 apiCall = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
 
 

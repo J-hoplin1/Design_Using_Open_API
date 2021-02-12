8b422f3e24835d9ea1f903f4d163f38a60b2213b
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-12 23:28:44 +0900

    Create initiateShell.sh

---
+++ Shell Script for Service/initiateShell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This Shell is written standard at Ubuntu
+
+echo "Check packages version and update."
+#설치된 패키지들 새로운 버전확인
+`sudo apt-get update`
+#패키지 최신버전 업그레이드
+`sudo apt-get upgrade`
+
+echo "Change server time standard at KST."
+#서버 시간을 KST로 재설정
+`sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime`
+echo "Start Service Scheduler"
+`python3 serviceScheduler.py`
+#스케줄러 Background로
+`bash processToBackground.sh`
+#최종적으로 백그라운드 프로세스 확인
+`ps -ef | grep python`

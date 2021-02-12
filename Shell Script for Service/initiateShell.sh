e207d4e2adf03f2f7863574cac696e4c6a986438
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-12 23:32:55 +0900

    Update initiateShell.sh

--- Shell Script for Service/initiateShell.sh
+++ Shell Script for Service/initiateShell.sh
@@ -8,6 +8,12 @@ echo "Check packages version and update."
 #패키지 최신버전 업그레이드
 `sudo apt-get upgrade`
 
+
+#pip install
+`sudo apt-get install python3-pip`
+#install require packages
+`pip3 install -r requirements.txt`
+
 echo "Change server time standard at KST."
 #서버 시간을 KST로 재설정
 `sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime`

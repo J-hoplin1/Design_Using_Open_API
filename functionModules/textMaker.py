a2663c04748ae498630ccca32a9165e6f2868088
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-12 14:28:24 +0900

    Update textMaker.py

--- functionModules/textMaker.py
+++ functionModules/textMaker.py
@@ -14,7 +14,7 @@ class makeText(object):
     
     def makeText(self):
         InfoText = "{} 코로나19 정보\n\n\n총 확진자 수 : {}명\n전체 사망자 수 : {}명\n전일대비 확진자 증가 수 :{}명\n전일대비 사망자 증가수 : {}명\n누적확진률 : {}%\n\n\n".format(self.data["dataDate"],self.data["data"]["totalDecidedPatient"],self.data["data"]["totalDeath"],self.data["data"]["todayDecidedPatient"],self.data["data"]["increasedDeath"],self.data["data"]["CumulatedConfirmPercentage"])
-        breifText = "<최신 보건복지부 브리핑>\n{} : {}\n{} : {}\n".format(self.data["data"]["mainBrief1"][0],self.data["data"]["mainBrief1"][1],self.data["data"]["mainBrief2"][0],self.data["data"]["mainBrief2"][1])
+        breifText = "<최신 보건복지부 브리핑>\n{} : {}\n{} : {}\n".format(self.data["data"]["briefTopics1"][0],self.data["data"]["briefTopics1"][1],self.data["data"]["briefTopics2"][0],self.data["data"]["briefTopics2"][1])
         issuesText = "<최신 핫이슈>\n {} : {}\n{} : {} \n\n\n\n\n\n\n\n데이터 제공 : 대한민국 행정안전부 공공데이터 포털\n서비스 제공자 : Hoplin (https://github.com/J-hoplin1)".format(self.data["data"]["issueTopics1"][0],self.data["data"]["issueTopics1"][1],self.data["data"]["issueTopics2"][0],self.data["data"]["issueTopics2"][1])
         completedText = InfoText + breifText + issuesText
         return completedText

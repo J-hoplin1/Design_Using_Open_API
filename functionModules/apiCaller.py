68df70d4772449410038d6c774f00900e153a520
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-13 23:12:07 +0900

    Update apiCaller.py

--- functionModules/apiCaller.py
+++ functionModules/apiCaller.py
@@ -44,7 +44,7 @@ class dataFromAPICall(object):
         self.apiKey = apiKey
         self.apiCall = apiCall
 
-    def buildRequests(self): 
+    def buildRequests(self) -> BeautifulSoup: 
         # 코드 실행한 시점
         executedPoint = datetime.now(timezone('Asia/Seoul'))
         endDate = executedPoint + timedelta(days = 1)# 하루뒤의 시간을 의미한다.
@@ -64,7 +64,8 @@ class dataFromAPICall(object):
         })
         response = requests.get(self.apiCall + queryParameter).text.encode('utf-8') # 기본적으로 requests를 인코딩한 반환값은 Byte String이 나오게 된다.
         response = response.decode('utf-8') # bytestring to Normal String
-        self.reProcessXML(response)
+        res = BeautifulSoup(response, 'lxml-xml')
+        return res
 
     def addMainNews(self) -> MutableSequence:
         covidSite = "http://ncov.mohw.go.kr/index.jsp"
@@ -84,8 +85,8 @@ class dataFromAPICall(object):
         sounds.append(hotIssues)
         return sounds
         
-    def reProcessXML(self,stringXML : str):
-        res = BeautifulSoup(stringXML, 'lxml-xml') # lxml-xml 매우빠르고 유일하게 지원되는 XML파서이다.
+    def reProcessXML(self,BSXML : BeautifulSoup) -> None:
+        res = BSXML# lxml-xml 매우빠르고 유일하게 지원되는 XML파서이다.
         item = res.findAll('item')
         if len(item) < 2:
             print("API data not updated yet. Try progress again after 10 minute")
@@ -118,6 +119,6 @@ class dataFromAPICall(object):
             dataDictionary['data']['issueTopics{}'.format(i)] = [o , hotIssues[o]]
         self.dumpToJSON(dataDictionary)
     
-    def dumpToJSON(self, dicInstance : MutableSequence):
+    def dumpToJSON(self, dicInstance : MutableSequence) -> None:
         with open('Datas/smtpSendDatas.json','w') as f:
             json.dump(dicInstance,f)

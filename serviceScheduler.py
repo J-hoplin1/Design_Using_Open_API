58b687458e072d09d55c8f4da04bfff9d9a47a67
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-14 10:42:31 +0900

    Error Logger

--- serviceScheduler.py
+++ serviceScheduler.py
@@ -46,8 +46,14 @@ class scheduler(object):
                 break
         
 def start():
-    schedulerInstance = scheduler()
-    schedulerInstance.startStream()
+    try:
+        schedulerInstance = scheduler()
+        raise Exception("테스트 예외클래스")
+        schedulerInstance.startStream()
+    except BaseException as e:
+        with open('Datas/ErrorLog.txt','a') as t:
+            t.write('Exception Occured at {}\nException msg : {}\n\n'.format(datetime.now(timezone('Asia/Seoul')).strftime("%Y/%m/%d %H : %M : %S"),e))
+        t.close()
             
 schedule.every().day.at("10:00").do(start)
 #schedule.every(10).seconds.do(start)

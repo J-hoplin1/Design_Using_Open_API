bd9eb0500fca0da2ae687594483c40d47865ea9b
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-15 22:36:34 +0900

    20210215 Patch

--- serviceTester.py
+++ serviceTester.py
@@ -6,7 +6,7 @@ from Datas.streamDatas import streamData
 from enum import Enum
 from urllib.parse import unquote
 
-options = Enum('option',['Service_Test','Add_Subscriber','End'])
+options = Enum('option',['Service_Test','Add_Subscriber','Delete_User','View_Subscriber_List','End'])
 
 def selectOpt() -> options:
     opt = [f'{p.value}. {p.name}' for p in options]
@@ -15,9 +15,17 @@ def selectOpt() -> options:
         for i in  opt:
             print(i)
         print('-' * 15)
-        select = int(input(">> "))
-        if 1<= select <= len(opt):
-            return options(select)
+        try:
+            select = int(input(">> "))
+            if 1<= select <= len(opt):
+                return options(select)
+        except ValueError:
+            pass
+        except KeyboardInterrupt:
+            print("\n비정상적인 종료입니다.")
+            pass
+        
+        
 apiKey = streamData.APIKEY
 apiURL = streamData.APIURL
 checker = patternChecker()
@@ -44,10 +52,43 @@ while True:
             pass
         else:
             subs['subscribers'].append(newSub)
+            subs['subscribers'] = list(set(subs['subscribers']))
             print("구독자 추가가 정상적으로 완료되었습니다.")
             with open('Datas/subs.json','w') as f:
                 json.dump(subs,f,indent = 4)
-            
+    
+    elif opt == options.View_Subscriber_List:
+        with open('Datas/subs.json','r') as f:
+            subs = json.load(f)
+        sublist = subs['subscribers']
+        print('\n' + '=' * 25)
+        for o,p in enumerate(sublist,start = 1):
+            print(f'{o}. {p}')
+        print('=' * 25 + "\n")
+    
+    elif opt == options.Delete_User:
+        with open('Datas/subs.json','r') as f:
+            subs = json.load(f)
+        sublist = subs['subscribers']
+        loop = True
+        while loop:
+            deleteSub = input("삭제하고자 하는 구독자의 이메일 입력하기('exit'을 입력하면 종료합니다) : ")
+            if deleteSub == "exit":
+                loop = False
+            elif not checker.checkEmailPattern(deleteSub):
+                print("올바르지 않은 이메일 형식입니다. 다시 입력해주시기 바랍니다.")
+                pass
+            else:
+                try:
+                    sublist.remove(deleteSub)
+                    subs['subscribers'] = sublist
+                    print("삭제가 정상적으로 완료되었습니다.")
+                    with open('Datas/subs.json','w') as f:
+                        json.dump(subs,f,indent = 4)
+                    loop = False
+                except ValueError:
+                    print("해당 이메일은 구독자 목록에 없습니다.")
+                    loop = False
     else:
         print("Service Close")
         break
